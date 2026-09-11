001471d81012de0444c4ad49525dd866912f37f1
stoicsatvik <buildwithsatvik@gmail.com>
2026-09-11 14:06:54 +0530

    Wire onboarding navigation

---
+++ agent/onboarding.js
@@ -0,0 +1,7 @@
+document.querySelector("#auditButton")?.addEventListener("click", () => {
+  location.href = "audit.html";
+});
+
+document.querySelector("#dashboardButton")?.addEventListener("click", () => {
+  location.href = "dashboard.html";
+});
